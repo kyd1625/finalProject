8fe1960c5a69505ab5af475eaddff39f2b610763
kyd1625 <rladudeh98@naver.com>
2022-12-09 18:51:18 +0900

    final DB

---
+++ 부산구군 테이블.sql
@@ -0,0 +1,247 @@
+DROP TABLE busanGG;
+CREATE TABLE busanGG(
+    GUGUNNM VARCHAR2(50),
+    DONG VARCHAR2(50)
+);
+
+SELECT * FROM busangg;
+
+INSERT INTO busangg VALUES ('중구','중앙동');
+INSERT INTO busangg VALUES ('중구','동광동');
+INSERT INTO busangg VALUES ('중구','대청동');
+INSERT INTO busangg VALUES ('중구','보수동');
+INSERT INTO busangg VALUES ('중구','부평동');
+INSERT INTO busangg VALUES ('중구','광복동');
+INSERT INTO busangg VALUES ('중구','남포동');
+INSERT INTO busangg VALUES ('중구','영주제1동');
+INSERT INTO busangg VALUES ('중구','영주제2동');
+commit;
+
+INSERT INTO busangg VALUES ('서구','동대신제1동');
+INSERT INTO busangg VALUES ('서구','동대신제2동');
+INSERT INTO busangg VALUES ('서구','동대신제3동');
+INSERT INTO busangg VALUES ('서구','서대신제1동');
+INSERT INTO busangg VALUES ('서구','서대신제3동');
+INSERT INTO busangg VALUES ('서구','서대신제4동');
+INSERT INTO busangg VALUES ('서구','부민동');
+INSERT INTO busangg VALUES ('서구','아미동');
+INSERT INTO busangg VALUES ('서구','초장동');
+INSERT INTO busangg VALUES ('서구','충무동');
+INSERT INTO busangg VALUES ('서구','남부민제1동');
+INSERT INTO busangg VALUES ('서구','남부민제2동');
+INSERT INTO busangg VALUES ('서구','암남동');
+commit;
+
+INSERT INTO busangg VALUES ('동구','초량제1동');
+INSERT INTO busangg VALUES ('동구','초량제2동');
+INSERT INTO busangg VALUES ('동구','초량제3동');
+INSERT INTO busangg VALUES ('동구','초량제6동');
+INSERT INTO busangg VALUES ('동구','수정제1동');
+INSERT INTO busangg VALUES ('동구','수정제2동');
+INSERT INTO busangg VALUES ('동구','수정제4동');
+INSERT INTO busangg VALUES ('동구','수정제5동');
+INSERT INTO busangg VALUES ('동구','좌천동');
+INSERT INTO busangg VALUES ('동구','범일제1동');
+INSERT INTO busangg VALUES ('동구','범일제2동');
+INSERT INTO busangg VALUES ('동구','범일제5동');
+commit;
+
+INSERT INTO busangg VALUES ('영도구','남항동');
+INSERT INTO busangg VALUES ('영도구','영선제1동');
+INSERT INTO busangg VALUES ('영도구','영선제2동');
+INSERT INTO busangg VALUES ('영도구','신선동');
+INSERT INTO busangg VALUES ('영도구','봉래제1동');
+INSERT INTO busangg VALUES ('영도구','봉래제2동');
+INSERT INTO busangg VALUES ('영도구','청학제1동');
+INSERT INTO busangg VALUES ('영도구','청학제2동');
+INSERT INTO busangg VALUES ('영도구','동삼제1동');
+INSERT INTO busangg VALUES ('영도구','동삼제2동');
+INSERT INTO busangg VALUES ('영도구','동삼제3동');
+commit;
+
+INSERT INTO busangg VALUES ('부산진구','부전제1동');
+INSERT INTO busangg VALUES ('부산진구','부전제2동');
+INSERT INTO busangg VALUES ('부산진구','연지동');
+INSERT INTO busangg VALUES ('부산진구','초읍동');
+INSERT INTO busangg VALUES ('부산진구','양정제1동');
+INSERT INTO busangg VALUES ('부산진구','양정제2동');
+INSERT INTO busangg VALUES ('부산진구','전포제1동');
+INSERT INTO busangg VALUES ('부산진구','전포제2동');
+INSERT INTO busangg VALUES ('부산진구','부암제1동');
+INSERT INTO busangg VALUES ('부산진구','부암제3동');
+INSERT INTO busangg VALUES ('부산진구','당감제1동');
+INSERT INTO busangg VALUES ('부산진구','당감제2동');
+INSERT INTO busangg VALUES ('부산진구','가야제1동');
+INSERT INTO busangg VALUES ('부산진구','가야제2동');
+INSERT INTO busangg VALUES ('부산진구','개금제1동');
+INSERT INTO busangg VALUES ('부산진구','개금제2동');
+INSERT INTO busangg VALUES ('부산진구','개금제3동');
+INSERT INTO busangg VALUES ('부산진구','범천제1동');
+INSERT INTO busangg VALUES ('부산진구','범천제2동');
+commit;
+
+INSERT INTO busangg VALUES ('동래구','수민동');
+INSERT INTO busangg VALUES ('동래구','복산동');
+INSERT INTO busangg VALUES ('동래구','명륜동');
+INSERT INTO busangg VALUES ('동래구','온천제1동');
+INSERT INTO busangg VALUES ('동래구','온천제2동');
+INSERT INTO busangg VALUES ('동래구','온천제3동');
+INSERT INTO busangg VALUES ('동래구','사직제1동');
+INSERT INTO busangg VALUES ('동래구','사직제2동');
+INSERT INTO busangg VALUES ('동래구','사직제3동');
+INSERT INTO busangg VALUES ('동래구','안락제1동');
+INSERT INTO busangg VALUES ('동래구','안락제2동');
+INSERT INTO busangg VALUES ('동래구','명장제1동');
+INSERT INTO busangg VALUES ('동래구','명장제2동');
+commit;
+
+INSERT INTO busangg VALUES ('남구','대연제1동');
+INSERT INTO busangg VALUES ('남구','대연제3동');
+INSERT INTO busangg VALUES ('남구','대연제4동');
+INSERT INTO busangg VALUES ('남구','대연제5동');
+INSERT INTO busangg VALUES ('남구','대연제6동');
+INSERT INTO busangg VALUES ('남구','용호제1동');
+INSERT INTO busangg VALUES ('남구','용호제2동');
+INSERT INTO busangg VALUES ('남구','용호제3동');
+INSERT INTO busangg VALUES ('남구','용호제4동');
+INSERT INTO busangg VALUES ('남구','용당동');
+INSERT INTO busangg VALUES ('남구','감만제1동');
+INSERT INTO busangg VALUES ('남구','감만제2동');
+INSERT INTO busangg VALUES ('남구','우암동');
+INSERT INTO busangg VALUES ('남구','문현제1동');
+INSERT INTO busangg VALUES ('남구','문현제2동');
+INSERT INTO busangg VALUES ('남구','문현제3동');
+INSERT INTO busangg VALUES ('남구','문현제4동');
+commit;
+
+INSERT INTO busangg VALUES ('해운대구','우제1동');
+INSERT INTO busangg VALUES ('해운대구','우제2동');
+INSERT INTO busangg VALUES ('해운대구','우제3동');
+INSERT INTO busangg VALUES ('해운대구','중제1동');
+INSERT INTO busangg VALUES ('해운대구','중제2동');
+INSERT INTO busangg VALUES ('해운대구','좌제1동');
+INSERT INTO busangg VALUES ('해운대구','좌제2동');
+INSERT INTO busangg VALUES ('해운대구','좌제3동');
+INSERT INTO busangg VALUES ('해운대구','좌제4동');
+INSERT INTO busangg VALUES ('해운대구','송정동');
+INSERT INTO busangg VALUES ('해운대구','반여제1동');
+INSERT INTO busangg VALUES ('해운대구','반여제2동');
+INSERT INTO busangg VALUES ('해운대구','반여제3동');
+INSERT INTO busangg VALUES ('해운대구','반여제4동');
+INSERT INTO busangg VALUES ('해운대구','반송제1동');
+INSERT INTO busangg VALUES ('해운대구','반송제2동');
+INSERT INTO busangg VALUES ('해운대구','재송제1동');
+INSERT INTO busangg VALUES ('해운대구','재송제2동');
+commit;
+
+INSERT INTO busangg VALUES ('북구','구포제1동');
+INSERT INTO busangg VALUES ('북구','구포제2동');
+INSERT INTO busangg VALUES ('북구','구포제3동');
+INSERT INTO busangg VALUES ('북구','금곡동');
+INSERT INTO busangg VALUES ('북구','화명제1동');
+INSERT INTO busangg VALUES ('북구','화명제2동');
+INSERT INTO busangg VALUES ('북구','화명제3동');
+INSERT INTO busangg VALUES ('북구','덕천제1동');
+INSERT INTO busangg VALUES ('북구','덕천제2동');
+INSERT INTO busangg VALUES ('북구','덕천제3동');
+INSERT INTO busangg VALUES ('북구','만덕제1동');
+INSERT INTO busangg VALUES ('북구','만덕제2동');
+INSERT INTO busangg VALUES ('북구','만덕제3동');
+commit;
+
+INSERT INTO busangg VALUES ('사하구','괴정제1동');
+INSERT INTO busangg VALUES ('사하구','괴정제2동');
+INSERT INTO busangg VALUES ('사하구','괴정제3동');
+INSERT INTO busangg VALUES ('사하구','괴정제4동');
+INSERT INTO busangg VALUES ('사하구','당리동');
+INSERT INTO busangg VALUES ('사하구','하단제1동');
+INSERT INTO busangg VALUES ('사하구','하단제2동');
+INSERT INTO busangg VALUES ('사하구','신평제1동');
+INSERT INTO busangg VALUES ('사하구','신평제2동');
+INSERT INTO busangg VALUES ('사하구','장림제1동');
+INSERT INTO busangg VALUES ('사하구','장림제2동');
+INSERT INTO busangg VALUES ('사하구','다대제1동');
+INSERT INTO busangg VALUES ('사하구','다대제2동');
+INSERT INTO busangg VALUES ('사하구','구평동');
+INSERT INTO busangg VALUES ('사하구','감천제1동');
+INSERT INTO busangg VALUES ('사하구','감천제2동');
+commit;
+
+INSERT INTO busangg VALUES ('금정구','서제1동');
+INSERT INTO busangg VALUES ('금정구','서제2동');
+INSERT INTO busangg VALUES ('금정구','서제3동');
+INSERT INTO busangg VALUES ('금정구','금사동');
+INSERT INTO busangg VALUES ('금정구','당리동');
+INSERT INTO busangg VALUES ('금정구','부곡제1동');
+INSERT INTO busangg VALUES ('금정구','부곡제2동');
+INSERT INTO busangg VALUES ('금정구','부곡제3동');
+INSERT INTO busangg VALUES ('금정구','부곡제4동');
+INSERT INTO busangg VALUES ('금정구','장전제1동');
+INSERT INTO busangg VALUES ('금정구','장전제2동');
+INSERT INTO busangg VALUES ('금정구','선두구동');
+INSERT INTO busangg VALUES ('금정구','청룡노포동');
+INSERT INTO busangg VALUES ('금정구','남산동');
+INSERT INTO busangg VALUES ('금정구','구서제1동');
+INSERT INTO busangg VALUES ('금정구','구서제2동');
+INSERT INTO busangg VALUES ('금정구','금성동');
+commit;
+
+INSERT INTO busangg VALUES ('강서구','대저1동');
+INSERT INTO busangg VALUES ('강서구','대저2동');
+INSERT INTO busangg VALUES ('강서구','강동동');
+INSERT INTO busangg VALUES ('강서구','명지1동');
+INSERT INTO busangg VALUES ('강서구','명지2동');
+INSERT INTO busangg VALUES ('강서구','가락동');
+INSERT INTO busangg VALUES ('강서구','녹산동');
+INSERT INTO busangg VALUES ('강서구','가덕도동');
+
+INSERT INTO busangg VALUES ('연제구','거제1동');
+INSERT INTO busangg VALUES ('연제구','거제2동');
+INSERT INTO busangg VALUES ('연제구','거제3동');
+INSERT INTO busangg VALUES ('연제구','거제4동');
+INSERT INTO busangg VALUES ('연제구','연산1동');
+INSERT INTO busangg VALUES ('연제구','연산2동');
+INSERT INTO busangg VALUES ('연제구','연산3동');
+INSERT INTO busangg VALUES ('연제구','연산4동');
+INSERT INTO busangg VALUES ('연제구','연산5동');
+INSERT INTO busangg VALUES ('연제구','연산6동');
+INSERT INTO busangg VALUES ('연제구','연산8동');
+INSERT INTO busangg VALUES ('연제구','연산9동');
+
+COMMIT;
+
+INSERT INTO busangg VALUES ('수영구','남천제1동');
+INSERT INTO busangg VALUES ('수영구','남천제2동');
+INSERT INTO busangg VALUES ('수영구','수영동');
+INSERT INTO busangg VALUES ('수영구','망미제1동');
+INSERT INTO busangg VALUES ('수영구','망미제2동');
+INSERT INTO busangg VALUES ('수영구','광안제1동');
+INSERT INTO busangg VALUES ('수영구','광안제2동');
+INSERT INTO busangg VALUES ('수영구','광안제3동');
+INSERT INTO busangg VALUES ('수영구','광안제4동');
+INSERT INTO busangg VALUES ('수영구','민락동');
+
+INSERT INTO busangg VALUES ('사상구','삼락동');
+INSERT INTO busangg VALUES ('사상구','모라1동');
+INSERT INTO busangg VALUES ('사상구','모라3동');
+INSERT INTO busangg VALUES ('사상구','덕포1동');
+INSERT INTO busangg VALUES ('사상구','덕포2동');
+INSERT INTO busangg VALUES ('사상구','괘법동');
+INSERT INTO busangg VALUES ('사상구','감전동');
+INSERT INTO busangg VALUES ('사상구','주례1동');
+INSERT INTO busangg VALUES ('사상구','주례2동');
+INSERT INTO busangg VALUES ('사상구','주례3동');
+INSERT INTO busangg VALUES ('사상구','학장동');
+INSERT INTO busangg VALUES ('사상구','엄궁동');
+
+INSERT INTO busangg VALUES ('기장군','기장읍');
+INSERT INTO busangg VALUES ('기장군','장안읍');
+INSERT INTO busangg VALUES ('기장군','정관읍');
+INSERT INTO busangg VALUES ('기장군','일광면');
+INSERT INTO busangg VALUES ('기장군','철마면');
+
+commit;
+
+SELECT
+    * FROM busangg;
+
